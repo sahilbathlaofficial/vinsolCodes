75afbcfb1b56db5f973dd3f6b0a554e4dab4b64d
Sahil Bathla <sahilbathla1@gmail.com>
2013-07-01 18:26:25 +0530

    changes to 2nd

--- javascript/Exercise 2/myScript.js
+++ javascript/Exercise 2/myScript.js
@@ -5,39 +5,49 @@
 */
 
 function checkLimit(self)
+{
+  var days = new Array(),  
+    count = 0,
+    checkBoxArray=document.getElementsByName('checkBox'),
+    length = checkBoxArray.length-1,
+    lastElement = length,
+    maxChecks = 3,
+    errorString = "";
+
+  for( i = 0;i < length;i = i + 1)
   {
-    var days=new Array();  
-    var count=0;
+    if(checkBoxArray[i] == self)
+    {
+      // make none deselect
+      checkBoxArray[lastElement].checked = false; 
+    }
 
-    for(i=0;i<7;i++)
+    else if(checkBoxArray[i].checked == true)
+    {
+      // add to array but not the selected checkbox
+      days[count++] = checkBoxArray[i].value;  
+    }
+
+    if(count==maxChecks && checkBoxArray[length].checked==false )
+    {
+      for (j = 0;j < maxChecks; j = j + 1)
       {
-        if(document.getElementsByName('checkBox')[i]==self)
-            {
-              // make none deselect
-              document.getElementsByName('checkBox')[7].checked=false; 
-            }
-
-        else if(document.getElementsByName('checkBox')[i].checked==true)
-            {
-              // add to array but not the selected checkbox
-              days[count++]=document.getElementsByName('checkBox')[i].value;  
-            }
-
-        if(count==3 && document.getElementsByName('checkBox')[7].checked==false )
-          {
-            alert("3 checkboxes already selected "+days[0]+" "+days[1]+" "+days[2]);
-            /* set self to be false if count>3 */
-            self.checked=false;  
-            break;
-          }
+        errorString = errorString + " " + days[j];
       }
+      alert("3 checkboxes already selected "+errorString);
+      // set self to be false if count>3 //
+      self.checked = false;  
+      break;
+    }
+  }
        
     
-    if(document.getElementsByName('checkBox')[7].checked==true && count>0)
-      {
-        for(i=0;i<7;i++)
-        document.getElementsByName('checkBox')[i].checked=false;
-      }
-        
-    
+  if(checkBoxArray[lastElement].checked == true && count > 0)
+  {
+    for(i = 0;i < length;i = i + 1)
+    {
+      checkBoxArray[i].checked=false;
+    }
   }  
+
+}

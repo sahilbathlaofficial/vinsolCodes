580d8aac6fcfb19878aae3642519e4dfe20760f6
Sahil Bathla <sahilbathla1@gmail.com>
2013-07-01 17:01:17 +0530

    changes to 2nd

--- javascript/Exercise 2/myScript.js
+++ javascript/Exercise 2/myScript.js
@@ -12,24 +12,32 @@ function checkLimit(self)
     for(i=0;i<7;i++)
       {
         if(document.getElementsByName('checkBox')[i]==self)
-          document.getElementsByName('checkBox')[7].checked=false; // make none deselect
+            {
+              // make none deselect
+              document.getElementsByName('checkBox')[7].checked=false; 
+            }
+
         else if(document.getElementsByName('checkBox')[i].checked==true)
-          days[count++]=document.getElementsByName('checkBox')[i].value;  // add to array but not the selected checkbox
+            {
+              // add to array but not the selected checkbox
+              days[count++]=document.getElementsByName('checkBox')[i].value;  
+            }
+
         if(count==3 && document.getElementsByName('checkBox')[7].checked==false )
           {
             alert("3 checkboxes already selected "+days[0]+" "+days[1]+" "+days[2]);
-            self.checked=false;  /* check self to be false if count>3 */
+            /* set self to be false if count>3 */
+            self.checked=false;  
             break;
           }
       }
-        if(count<3)
-        days[count++]=self.value; // add the selected checkbox to array end if count < 3
-   
-        if(document.getElementsByName('checkBox')[7].checked==true && count>0)
-          {
-            for(i=0;i<7;i++)
-            document.getElementsByName('checkBox')[i].checked=false;
-          }
+       
+    
+    if(document.getElementsByName('checkBox')[7].checked==true && count>0)
+      {
+        for(i=0;i<7;i++)
+        document.getElementsByName('checkBox')[i].checked=false;
+      }
         
     
-  }
+  }  

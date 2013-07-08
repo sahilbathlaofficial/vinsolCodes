69483003d7a6bc8e02e3fdf4908aeb6a8c203bc1
Sahil Bathla <sahilbathla1@gmail.com>
2013-07-08 18:22:49 +0530

    Prompt url exercise

--- javascript/Exercise 8/promptUrl.js
+++ javascript/Exercise 8/promptUrl.js
@@ -13,7 +13,6 @@ window.onload = function ()
  var url = prompt('Enter url to open (i.e. xyz.com)','google.com');
  url = "http://www." + url;
  var newWindow = window.open(url,'_blank','width=400,height=450,menubar=no,scrollbars=no,status=0,toolbar=0,titlebar=0');
-  newWindow.document.body.style.overflow = 'hidden';
   
 }
 

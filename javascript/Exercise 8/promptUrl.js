9849619b11b70e548cc18a8324d56575d3046a3a
Sahil Bathla <sahilbathla1@gmail.com>
2013-07-10 13:21:02 +0530

    changes

--- javascript/Exercise 8/promptUrl.js
+++ javascript/Exercise 8/promptUrl.js
@@ -10,9 +10,15 @@
 window.onload = function () 
 {
 
- var url = prompt('Enter url to open (i.e. xyz.com)','google.com');
- url = "http://www." + url;
- var newWindow = window.open(url,'_blank','width=400,height=450,menubar=no,scrollbars=no,status=0,toolbar=0,titlebar=0');
-  
+  var url = prompt('Enter url to open (i.e. xyz.com)','google.com');
+  if(url !== '')
+  {
+    url = "http://www." + url;
+    var newWindow = window.open(url,'_blank','width=400,height=450,menubar=no,scrollbars=no,status=0,toolbar=0,titlebar=0');
+  }
+  else
+  {
+    alert("No Url Entered");
+  }
 }
 

8e3a3f5efa92e5be3bda229ce43bf66aa6f0df27
Sahil Bathla <sahilbathla1@gmail.com>
2013-07-11 11:29:49 +0530

    Changes to country exercise

--- javascript/Exercise 9/addRemoveCountries.js
+++ javascript/Exercise 9/addRemoveCountries.js
@@ -5,26 +5,23 @@
 */
 
 // Class for Lists
-function List(list)
-{
-  this.list = list;
-  this.addInList = function(selectedItem)
-  {
-    this.list.add(selectedItem);
-  }
+function List(list) {
+    this.list = list;
+    this.addInList = function (selectedItem) {
+        this.list.add(selectedItem);
+    };
 }
 
 
 // function to get the selected items and put them in corresponding list
-function shiftSelected(listFrom,listTo)
-{
-  listFrom = new List(document.getElementById(listFrom));
-  listTo = new List(document.getElementById(listTo));
-  var selectedItemsArray = [];
-  while(listFrom.list.selectedIndex != -1 )
-  {
-    listTo.addInList(listFrom.list[listFrom.list.selectedIndex]);
-  }
+function shiftSelected(listFrom, listTo) {
+    listFrom = new List(document.getElementById(listFrom));
+    listTo = new List(document.getElementById(listTo));
+    var selectedItemsArray = [];
+    while (listFrom.list.selectedIndex !== -1)
+    {
+      listTo.addInList(listFrom.list[listFrom.list.selectedIndex]);
+    }
 }
 
 

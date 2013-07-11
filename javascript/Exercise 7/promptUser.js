3c2cf63e17bf664f14071ca54b425c36d6c2c590
Sahil Bathla <sahilbathla1@gmail.com>
2013-07-11 15:46:36 +0530

    common function made for firstname and lastname

--- javascript/Exercise 7/promptUser.js
+++ javascript/Exercise 7/promptUser.js
@@ -4,48 +4,28 @@
   Version - 1.0
 */
 
+function promptName(promptText)
+{
+  var name = '';
+  do
+  {
+    name = prompt("Please enter your " + promptText);
+    if(name === '' || name.trim().length === 0 )
+    {
+      alert(promptText + " cannnot be empty")
+    }
+  }while(name === '' || name.trim().length === 0);
 
+  return name;
+}
 
 
 window.onload = function () 
 {
 
-  var finalString = 'Hello',
-      stripedLastName ='',
-      stripedFirstName = '',
-      lastName = '',
-      firstName = '';
- 
-  // for first name
-  do
-  {
-    firstName = prompt("Please enter your first name:","Your first name");
-    stripedFirstName = firstName.replace(/ /g,'');
-    if(firstName === '' || stripedFirstName.length === 0 )
-    {
-      alert("First Name cannnot be empty")
-    }
-    else 
-    {
-    finalString = finalString + " " + firstName;
-    }
-  }while(firstName === '' || stripedFirstName.length === 0);
-
-  //for last name
-  do
-  {
-    lastName = prompt("Please enter your last name:","Your last name");
-    stripedLastName = lastName.replace(/ /g,'');
-    if(lastName === '' || stripedLastName.length === 0 )
-    {
-      alert("Last Name cannnot be empty")
-    }
-    else 
-    {
-    finalString = finalString + " " + lastName;
-    }
-  }while(lastName === '' || stripedLastName.length === 0);
- 
+  var finalString = 'Hello ',
+  finalString = finalString + promptName('First Name') + ' ';
+  finalString = finalString + promptName('Last Name'); 
   alert(finalString);
   
 }

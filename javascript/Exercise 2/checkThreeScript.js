4d99384416be60d1a50f96fc6a79d4dd07172404
Sahil Bathla <sahilbathla1@gmail.com>
2013-07-11 15:34:26 +0530

    Uncheck handled

--- javascript/Exercise 2/checkThreeScript.js
+++ javascript/Exercise 2/checkThreeScript.js
@@ -16,8 +16,15 @@ function checkLimit(self)
     maxChecks = 3,
     errorString = "";
 
+  if(checkBoxArray[lastElement] !== self && self.checked === false)
+    {
+      //remove item from days array if any element leaving None is unchecked
+      days.splice(days.indexOf(self.value),1);
+    }
+
+
   // if NONE is checked make all elements unchecked
-  if(checkBoxArray[lastElement].checked == true && days.length > 0)
+  else if(checkBoxArray[lastElement].checked == true && days.length > 0)
   {
     days = [];
     for(i = 0;i < length;i = i + 1)
@@ -26,23 +33,10 @@ function checkLimit(self)
     }
   }
 
-  else if(days.length == maxChecks )
+  else if(days.length === maxChecks )
   {
     // generating error message
-    for (j = 0;j < maxChecks; j = j + 1)
-    {
-      if(j == maxChecks - 1)
-      {
-        // remove the last comma before 'and'
-        errorString = errorString.slice(0,-1);
-        errorString = errorString + " and " + days[j];
-      }
-      else
-      {
-        errorString = errorString + days[j]  + ",";
-      }
-    }
-
+    errorString = errorString + days[0]  + "," + days[1] + " and " + days[2]; 
     // set self to be false if count>3 
     self.checked = false;  
     alert("Only 3 days can be selected. You have already selected " + errorString);
@@ -50,10 +44,11 @@ function checkLimit(self)
 
   else
   {
-    if(checkBoxArray[lastElement] != self )   
+    if(checkBoxArray[lastElement] !== self && self.checked === true )   
     {
+      // insert values in days array if not NONE and checked
       days.push(self.value);
-      // make None unchecked
+      // and make None unchecked
       checkBoxArray[lastElement].checked = false;
     }
     

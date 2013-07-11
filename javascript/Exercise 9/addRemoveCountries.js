1401d293534be64a1a9d67a1a5f9250fafe2d038
Sahil Bathla <sahilbathla1@gmail.com>
2013-07-11 11:23:09 +0530

    Changes to country exercise

--- javascript/Exercise 9/addRemoveCountries.js
+++ javascript/Exercise 9/addRemoveCountries.js
@@ -8,42 +8,22 @@
 function List(list)
 {
   this.list = list;
-  this.appendInList = function(selectedItemArray)
+  this.addInList = function(selectedItem)
   {
-    for(i = 0; i < selectedItemArray.length; i = i + 1)
-    {
-      this.list.appendChild(selectedItemArray[i]);
-    } 
+    this.list.add(selectedItem);
   }
-}
-
-// create two global List objects
-window.onload = function()
-{
-  firstList = new List(document.getElementById('firstList'));
-  secondList = new List(document.getElementById('secondList'));
 }
 
 
 // function to get the selected items and put them in corresponding list
-function shiftSelected(listId)
+function shiftSelected(listFrom,listTo)
 {
+  listFrom = new List(document.getElementById(listFrom));
+  listTo = new List(document.getElementById(listTo));
   var selectedItemsArray = [];
-  var selectedItems = document.getElementById(listId);
-  for(i = 0;i < selectedItems.length; i = i + 1)
+  while(listFrom.list.selectedIndex != -1 )
   {
-    if(selectedItems[i].selected === true )
-    {
-      selectedItemsArray.push(selectedItems[i]);
-    }    
-  }
-  if(listId === 'firstList')
-  {
-    secondList.appendInList(selectedItemsArray);
-  }
-  else
-  {
-    firstList.appendInList(selectedItemsArray);
+    listTo.addInList(listFrom.list[listFrom.list.selectedIndex]);
   }
 }
 

2abf2df6d818316ace4c246dd26d804bdfc63b15
Sahil Bathla <sahilbathla1@gmail.com>
2013-07-12 12:56:59 +0530

    Using only regex

--- javascript/Exercise 12/domainExtract.js
+++ javascript/Exercise 12/domainExtract.js
@@ -1,7 +1,7 @@
 /*
   Author - Sahil Bathla
   Description - Domain/Sub-Domain extraction
-  Version - 1.0
+  Version - 2.0
 */
 
 // Class for Form
@@ -9,27 +9,23 @@
 function Form(formObject)
 {
   this.form = formObject;
-  var regexURL = /^(http[s]?:\/\/)?(www.)?[a-zA-Z0-9]+(\.[a-zA-Z0-9]+)*\.[a-z.]{2,6}[/]?([\w]+[/]{0,1})*[^/]*$/;
+  var regexURL = /^(?:http[s]{0,1}:\/\/)?(?:www.)?([.a-zA-Z0-9]*)?(?:\.)([a-zA-Z0-9]+\.[a-z.]{2,6})(?:\/)?(?:[\w]+[/]{0,1})*(?:[^/])*$/;
   var hostName = '',
-      domain = '',
-      subDomain = '';
 
   this.extractDomain = function() 
   {
+
     if(regexURL.test(this.form.elements[0].value) === false)
     {
       alert("Enter a valid URl");
     }
     else
     {
-    var hostName = this.form.elements[0].value.replace(/^(http[s]*:\/\/)?(www.)?/,'').replace(/\/.*$/,'');
-    var domain= hostName.slice(hostName.search(/[\w]+\.[a-z.]{2,4}$/),hostName.length);
-    alert("Domain: " + domain);
-    var subDomain =  hostName.slice(0,hostName.search(/[\w]+\.[a-z.]{2,4}$/)).slice(0,-1);
-    if(subDomain !== '')
-    {
-      alert("Sub Domain: " + subDomain);
-    }
+      // Extracting Domain and subdomain using capturing mode
+      hostName = (regexURL).exec(this.form.elements[0].value);
+      alert("Domain: " + hostName[2]);
+      if(hostName[1] !== undefined)
+      alert("Sub Domain: " + hostName[1]);
     }
   }
 }

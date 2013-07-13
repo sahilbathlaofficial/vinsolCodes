cf38625e9e078c1ab137d1c79e61ebdbf3dbb707
Sahil Bathla <sahilbathla1@gmail.com>
2013-07-13 17:06:14 +0530

    Hidden Exercise done:

--- javascript/Hidden Exercise 1/autoComplete.js
+++ javascript/Hidden Exercise 1/autoComplete.js
@@ -9,41 +9,41 @@
 function Names(person)
 {
   this.names = person;
-  this.suggestBox= document.getElementById('suggestBox');
-  this.resultBox= document.getElementById('suggestionResults');
+  var suggestBox= document.getElementById('suggestBox');
+  var resultBox= document.getElementById('suggestionResults');
   var flag = false;
   this.suggest = function() 
     {
-      this.clearResultBox();
+      this.clearResult();
       flag = false;
-      if(this.suggestBox.value !== '')
+      if(suggestBox.value !== '')
       {
         for (i = 0 ; i < this.names.length ; i = i + 1)
         {
-          if((this.names[i]["name"].toLowerCase()).indexOf(this.suggestBox.value.toLowerCase()) === 0 )
+          if((this.names[i]["name"].toLowerCase()).indexOf(suggestBox.value.toLowerCase()) === 0 )
           {
-           para = document.createElement('p');
-           para.appendChild(document.createTextNode('' + this.names[i]["name"]));
-           para.onclick = function ()
-           {
+            para = document.createElement('p');
+            para.appendChild(document.createTextNode('' + this.names[i]["name"]));
+            para.onclick = function ()
+            {
               document.getElementById('suggestBox').value = this.textContent;
-           }
-           this.resultBox.appendChild(para);
+            }
+            resultBox.appendChild(para);
             flag = true;
           }
         }
 
         if(flag === true)
         {
-          this.resultBox.style.border = 'solid 1px black';
+          resultBox.style.border = 'solid 1px black';
         }
       }
     }
 
-  this.clearResultBox = function()
+  this.clearResult = function()
   {
-    this.resultBox.innerHTML = '';
-    this.resultBox.style.border = 'none';
+    resultBox.innerHTML = '';
+    resultBox.style.border = 'none';
   }
 }
 
@@ -51,17 +51,17 @@ window.onload = function()
 {
   person =[{"name":"Luigi Damiano"}, {"name":"Zenith Coboro"}, {"name":"Zig Ziglar"}, {"name":"Steve Costner"}, {"name":"Bill Grazer"}, {"name":"Timothy Frazer"}, {"name":"Boris Becker"}, {"name":"Glenn Gladwich"}, {"name":"Jim Jackson"}, {"name":"Aaron Kabin"}, {"name":"Roy Goldwin"}, {"name":"Jason Goldberg"}, {"name":"Tim Ferris"}, {"name":"Buck Singham"}, {"name":"Malcom Gladwell"}, {"name":"Joy Rabura"}, {"name":"Vid Luther"}, {"name":"Tom Glicken"}, {"name":"Ray Baxter"}, {"name":"Ari Kama"}, {"name":"Kenichi Suzuki"}, {"name":"Rick Olson"}]
 
-  nameObject = new Names(person);
+  persons = new Names(person);
 }
 
 function suggestName()
 {
-  nameObject.suggest();
+  persons.suggest();
 }
 
 function clearResults()
 {
-  nameObject.clearResultBox();
+  persons.clearResult();
 }
 
 

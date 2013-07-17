e5b4b41433deb9e2681703810814c80fdd25b5ad
Sahil Bathla <sahilbathla1@gmail.com>
2013-07-17 17:44:01 +0530

    Exercise 6

--- jQuery/Exercise 6/js/blog.js
+++ jQuery/Exercise 6/js/blog.js
@@ -2,7 +2,7 @@
 Author - Sahil Bathla
 Description : -
     Clicking on a headline in the #blog div should slide down the excerpt paragraph
-    Clicking on another headline should slide down its excerpt paragraph, and slide up any other currently showing excerpt paragraphs.
+    Clicking on another headline should slide down its excerpt paragraph, and slide up any other currently showing excerpt paragraphs..
 */
 
 

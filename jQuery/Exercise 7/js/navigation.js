89ab4eac0fd5d3cfce529552a0cdea5215969803
Sahil Bathla <sahilbathla1@gmail.com>
2013-07-17 18:32:18 +0530

    Exercise 7

---
+++ jQuery/Exercise 7/js/navigation.js
@@ -0,0 +1,21 @@
+/*
+Author - Sahil Bathla
+Description : -
+
+    Hovering over an item in the main menu should show that item's submenu items, if any.
+
+    Exiting an item should hide any submenu items.
+*/
+
+
+$(document).ready(function()
+{
+
+  $('#nav li').hover( function()
+  {
+    $(this).toggleClass('hover').children('ul').toggle();
+  });
+
+});
+
+

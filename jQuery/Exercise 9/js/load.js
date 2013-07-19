a3b70d1d77914455cf13bec5ef65d681c23f9c89
Sahil Bathla <sahilbathla1@gmail.com>
2013-07-19 15:33:52 +0530

    Exercise 9

---
+++ jQuery/Exercise 9/js/load.js
@@ -0,0 +1,24 @@
+/*
+Author - Sahil Bathla
+Description : - AJAX Load
+*/
+
+$(document).ready(function()
+{
+  $('#blog h3').each( function(){
+  $(this).after('<div></div>');
+  reference = $(this).next();
+  $.data(this,'ref', reference); 
+
+  $(this).click( function(event)
+  {
+    event.preventDefault();
+    target = 'data/'+ $(this).children('a').attr('href');
+    $.data(this,'ref').load(target);
+  });
+
+  });
+ 
+});
+
+

9ff6b742de208a4faa7d89e8cb7daefff8ddeb34
Sahil Bathla <sahilbathla1@gmail.com>
2013-07-23 16:55:16 +0530

    Changes made

--- jQuery/Exercise 10/js/specials.js
+++ jQuery/Exercise 10/js/specials.js
@@ -5,6 +5,8 @@ Description : - Load Content with JSON
 
 $(document).ready(function()
 {
+  var dataJSON = []
+  var optionSelected = '';
   // cache the JSON data into a variable dataJSON
   $.ajax({url: 'data/specials.json',dataType: 'json', success : function(data)
     {
@@ -12,16 +14,15 @@ $(document).ready(function()
     }}).done(function() { console.log( "success" ); }).fail(function() { console.log("error"); })
 
 
-  // create target div  
-  $('#specials form').after('<div id="targetDiv"></div>');
-  
-  // remove submit form 
-   $('#specials form').find(':input[type="submit"]').parent().remove();
+  // create target div  & remove submit form 
+  $('#specials form').after('<div id="targetDiv"></div>').find(':input[type="submit"]').parent().remove();
 
   // On change add JSON Data corresponding to day
   $('#specials form select').bind('change',function() {
-  var optionSelected = $('#specials form  option:selected').text().toLowerCase();
-  $('#targetDiv').html(dataJSON[optionSelected].title + '<br />' + dataJSON[optionSelected].text);  
+  optionSelected = $('#specials form  option:selected').text().toLowerCase();
+
+  // html('') in case user selects the default (select) which is not in JSON data
+  $('#targetDiv').html('').html(dataJSON[optionSelected].title + '<br />' + dataJSON[optionSelected].text);  
   });
   
  

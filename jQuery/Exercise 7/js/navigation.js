aa68b313e19aa069fc1201e36f67420e2a14b334
Sahil Bathla <sahilbathla1@gmail.com>
2013-07-23 12:21:44 +0530

    Toggle class added

--- jQuery/Exercise 7/js/navigation.js
+++ jQuery/Exercise 7/js/navigation.js
@@ -13,7 +13,7 @@ $(document).ready(function()
 
   $('#nav li').hover( function()
   {
-    $(this).toggleClass('hover').children('ul').toggle();
+    $(this).toggleClass('hover').children('ul').toggleClass('hover');
   });
 
 });

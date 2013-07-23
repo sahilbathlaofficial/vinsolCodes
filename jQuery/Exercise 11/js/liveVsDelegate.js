e31c91e6cce811c87dae7d72c9fa401a21943cab
Sahil Bathla <sahilbathla1@gmail.com>
2013-07-23 12:05:08 +0530

    Changes as asked

--- jQuery/Exercise 11/js/liveVsDelegate.js
+++ jQuery/Exercise 11/js/liveVsDelegate.js
@@ -6,19 +6,19 @@ Description : - Load Content with JSON
 $(document).ready(function()
 {
   var counter = 1, id = '';
-  $('#container').append('<input type="button"></input>');
+  $('#container').append('<input type="button" id="addItem" value="Add Item"></input>');
   $('#container').append('<h2>Top of Stack</h2>');
-  $('#container').children().eq(0).attr({id:'addItem',value:'Add Item'}).click( function()
+  $('#container :nth-child(1)').click( function()
   {
-    id = "divStack" + counter;
+    divClass = "divStack";
     itemText = "Stack Item #" + counter;
-    $('#container h2').after('<div></div>');
-    $('#container').children('div:first').attr('id',id).text(itemText);
+    divHTML = '<div class="' + divClass + '">' + itemText + '</div>';
+    $('#container h2').after(divHTML);
     counter = counter + 1;
   });
 
-  $('#container').delegate('div:first','click',function() {$(this).remove();});
-  $('#container').delegate('div','click',function() { $(this).toggleClass('highlight'); });
+  $('#container').delegate('.divStack:first','click',function() {$(this).remove();});
+  $('#container').delegate('.divStack','click',function() { $(this).toggleClass('highlight'); });
   
 });
 

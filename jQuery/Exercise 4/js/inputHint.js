085d0cd75c331f494b44ecd0b937e03ab85d31a0
Sahil Bathla <sahilbathla1@gmail.com>
2013-07-23 15:33:16 +0530

    Changes to inputField Question

--- jQuery/Exercise 4/js/inputHint.js
+++ jQuery/Exercise 4/js/inputHint.js
@@ -7,36 +7,30 @@ Description : -
     Bind a focus event to the search input that removes the hint text and the "hint" class
     Bind a blur event to the search input that restores the hint text and "hint" class if no search text was entered
 */
-function toggleValue(originalValue,valueToToggle)
-{
-  if (originalValue === '')
-   {
-      return valueToToggle;
-   }
-   else
-   {
-       return '';
-   }
-}
 
 $(document).ready(function()
 {
-  hintText = $('label[for="q"]').text();
-  //Set the value of the search input to the text of the label element
-  $(':input[name="q"]').val(hintText);
-  //Add a class of "hint" to the search input
-  $(':input[name="q"]').addClass('hint');
+  var hintText = $('label[for="q"]').text();
+  /*Set the value of the search input to the text of the label element 
+    Add a class of "hint" to the search input
+    Bind a focus event to the search input that removes the hint text and the "hint" class
+  */
+  $(':input[name="q"]').val(hintText).addClass('hint').bind('focus blur',function() 
+  {
+      if ($(this).hasClass('hint'))
+      {
+        $(this).val('');
+        $(this).removeClass('hint');  
+      }
+      else if( $(this).val().trim().length === 0)
+      {
+        $(this).val(hintText);
+        $(this).addClass('hint'); 
+      }
+  });
   //Remove the label element
   $('label[for="q"]').remove();
-  // Bind a focus event to the search input that removes the hint text and the "hint" class
-  $(':input[name="q"]').bind('focus blur',function() 
-  {
-    if ($(this).val().trim() === '' || $(this).val() === hintText)
-    {
-      $(this).val(toggleValue($(this).val(),hintText));
-      $(this).toggleClass('hint');
-    }
-  });
+ 
 });
 
 

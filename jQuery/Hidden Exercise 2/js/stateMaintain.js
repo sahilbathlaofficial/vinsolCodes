8cfb8c104d672aac6f120c37bed3876e9ca87c7b
Sahil Bathla <sahilbathla1@gmail.com>
2013-08-06 15:04:20 +0530

    Changes made no active class used

--- jQuery/Hidden Exercise 2/js/stateMaintain.js
+++ jQuery/Hidden Exercise 2/js/stateMaintain.js
@@ -1,19 +1,33 @@
-
-$(document).ready( function(event) {
+//1st part of hash denotes the parent and second part denotes the child
+$(window).load( function(event) {
   //find the hash
   hash = window.location.hash.replace('#','');
   //if hash not null
   if(hash !== '' )
   {
-    $('a:contains("' + hash + '")').addClass('selectedLink active');
+    var parent = hash.split('c')[0];
+    var child = hash.split('c')[1];
+    $('.dcjq-parent').each( function(index) {
+      if( index === parseInt(parent,10))
+      {
+        $(this).parents('.accordion').show();
+        $(this).next().show().find('a').eq(parseInt(child,10)).addClass('selectedLink');
+      }
+    });
   }
 
     $('.accordion li a').click( function(event) { 
         $('.selectedLink').removeClass('selectedLink');
-        window.location.hash =  $(this).text();
-        if(!$(this).hasClass('dcjq-parent'))
+        if ($(this).hasClass('dcjq-parent'))
         {
-          $('a:contains("' + $(this).text() + '")').addClass('selectedLink active');
+          index = $('.dcjq-parent').index($(this));
+          window.location.hash =  index;
+        }
+        else
+        {
+          // find index of this clicked a which has no further links
+          window.location.hash = window.location.hash + 'c' + $(this).closest('.accordion').find('a').index($(this));
+          $(this).addClass('selectedLink');
           event.preventDefault();
           window.location = $(this).attr('href') + window.location.hash;
         }

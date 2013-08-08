08b573a3c09a41534f38580b370f869f07f5d7db
Sahil Bathla <sahilbathla1@gmail.com>
2013-08-08 13:02:06 +0530

    Exercise 4

--- Basic_Ruby/Exercise 4/Palindrome.rb
+++ Basic_Ruby/Exercise 4/Palindrome.rb
@@ -1,17 +1,16 @@
 inputString = ''
 characterInput = ''
-while characterInput != 'q'
+while (characterInput != 'q' &&  characterInput != 'Q')
   begin
     system("stty raw echo")
     characterInput = STDIN.getc
-    inputString << characterInput if characterInput =~ /qQ/
+    inputString << characterInput if (characterInput != 'q' &&  characterInput != 'Q')
   ensure
     system("stty -raw echo")
   end
 end
-
 if inputString == inputString.reverse 
-  puts "\nPalindrome"
+  puts "\n#{inputString} is a Palindrome"
 else
   puts "\nNot a Palindrome"
 end

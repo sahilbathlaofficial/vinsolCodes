6cecffe87c9c8d1debd42e4d5a708c19f616a638
Sahil Bathla <sahilbathla1@gmail.com>
2013-08-08 11:01:12 +0530

    approach changed

--- Basic_Ruby/Exercise 2/Fibonacci.rb
+++ Basic_Ruby/Exercise 2/Fibonacci.rb
@@ -1,12 +1,13 @@
 def fibonacci
-  limit = 1000
-  print '0 1'
-  yield(0,1,limit)
+  print "0 1"
+  first = 0
+  second = 1
+  while (first + second) < 1000
+    third = first + second
+    yield(third)
+    first = second
+    second = third
+  end
 end
-fibonacci do |first,second,limit|
- while (first + second) < 1000 
-   print " #{first+second}"
-   second = first + second
-   first = second
- end 
-end
+fibonacci {|sum| print " #{sum}"}
+

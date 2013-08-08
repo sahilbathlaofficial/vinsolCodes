55a052679f5f35f17bb4db687d8a8108d99b26a0
Sahil Bathla <sahilbathla1@gmail.com>
2013-08-08 12:41:50 +0530

    Palindrome

---
+++ Basic_Ruby/Exercise 4/Palindrome.rb
@@ -0,0 +1,17 @@
+inputString = ''
+characterInput = ''
+while characterInput != 'q'
+  begin
+    system("stty raw echo")
+    characterInput = STDIN.getc
+    inputString << characterInput if characterInput != 'q'
+  ensure
+    system("stty -raw echo")
+  end
+end
+
+if inputString == inputString.reverse 
+  puts "\nPalindrome"
+else
+  puts "\nNot a Palindrome"
+end

9679e1ad3202dc1dc4f3799cb0c81c9876159a6b
Sahil Bathla <sahilbathla1@gmail.com>
2013-08-08 10:46:55 +0530

    Changes

--- Basic_Ruby/Exercise 1/Occurence_Hash.rb
+++ Basic_Ruby/Exercise 1/Occurence_Hash.rb
@@ -1,15 +1,5 @@
 hashAlphabets = Hash.new(0)
 inputString = gets
-i = 0
-while i < inputString.length - 1
-  if inputString[i] =~ /[a-zA-Z]/
-  hashAlphabets[inputString[i]] += 1
-  end
-i += 1
-end
-i = 0
-while i < hashAlphabets.keys.size
-  puts "#{hashAlphabets.keys[i]}-#{hashAlphabets[inputString[i]]}"
-i += 1
-end
+inputString.each_char { |alphabet| hashAlphabets[alphabet] += 1 if alphabet =~ /[a-zA-Z]/ }
+hashAlphabets.each { |key,value| puts "#{key} - #{value}"}
 

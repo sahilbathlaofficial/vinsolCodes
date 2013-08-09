b3f14a3ba67dc6d4f0ceaa8faf9530feebdba4c5
Sahil Bathla <sahilbathla1@gmail.com>
2013-08-09 11:47:37 +0530

    Customer class

---
+++ Basic_Ruby/Exercise 5/Customer_Class.rb
@@ -0,0 +1,24 @@
+class Customer
+  attr_reader :name,:account_id,:balance;
+  @@account_id = 0;
+  def initialize (name)
+    @name = name
+    @@account_id = @@account_id + 1;
+    @balance = 1000
+  end
+  def deposit(amount)
+    @balance = @balance + amount
+    puts "Amount Deposited #{amount} by #{@name} New balance = #{@balance}"
+  end
+  def withdraw(amount)
+    @balance = @balance - amount
+    puts "Amount Withdrawn #{amount} by #{@name} New balance = #{@balance}"
+  end
+end
+
+account_id = 0
+john = Customer.new('john')
+ron = Customer.new('ron')
+john.deposit(200)
+ron.withdraw(100)
+

18da80e5c15a4d8383f522e787760f70b33400bd
Sahil Bathla <sahilbathla1@gmail.com>
2013-08-12 12:48:46 +0530

    changes made

---
+++ Basic_Ruby/Exercise 4/bin/palindrome.rb
@@ -0,0 +1,12 @@
+while true
+  begin
+    system("stty raw echo")
+    characterInput = STDIN.getc
+    break if (characterInput.to_s =~ /q|Q/)
+    inputString = (inputString || '') << characterInput
+  ensure
+    system("stty -raw echo")
+  end
+end
+print "\n#{inputString}",inputString == inputString.reverse ?  " is a Palindrome" : " is not a Palindrome"
+

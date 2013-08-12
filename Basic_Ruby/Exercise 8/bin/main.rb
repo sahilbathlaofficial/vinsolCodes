c70189510a5c8e045471153fd412a23ad6db1f2b
Sahil Bathla <sahilbathla1@gmail.com>
2013-08-12 17:20:56 +0530

    power of array

---
+++ Basic_Ruby/Exercise 8/bin/main.rb
@@ -0,0 +1,3 @@
+#! /usr/bin/ruby
+require_relative '../lib/array'
+print [2.1,[3,[2.0,2],34],"sahil",3].power(4)
--- Basic_Ruby/Exercise 8/lib/array.rb
+++ Basic_Ruby/Exercise 8/lib/array.rb
@@ -1,8 +1,15 @@
 class Array
   def power(raise_to)
-    self.collect { |element| element ** raise_to }
+    self.collect do |element|
+      if element.is_a? Numeric
+        element = element ** raise_to  
+      elsif element.is_a? Array
+        element.power(raise_to)
+      else
+        element = element.to_s + "[Power function not applicable on this type]"
+      end 
+    end
   end
 end
 
-puts "Sdsd"
 

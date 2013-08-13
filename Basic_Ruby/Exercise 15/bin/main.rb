71bd148e51e4edea4c67180a729445210c0792c3
Sahil Bathla <sahilbathla1@gmail.com>
2013-08-13 18:39:04 +0530

    Prime Numbers upto n

---
+++ Basic_Ruby/Exercise 15/bin/main.rb
@@ -0,0 +1,9 @@
+#! /usr/bin/ruby
+require 'prime'
+require_relative "../lib/integer"
+limit = gets while (limit = limit.to_i) == 0
+puts limit < 2 ? "No prime number" : "Prime Numbers \n2"
+3.step(limit,2).each {|number| puts number if number.check_prime } if limit > 2
+
+
+
---
+++ Basic_Ruby/Exercise 15/lib/integer.rb
@@ -0,0 +1,5 @@
+class Integer
+  def check_prime
+    Prime.prime?(self)
+  end
+end

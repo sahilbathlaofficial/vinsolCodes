8ba7dbeee8a74465dfab26209d0acfd041d3edbc
Sahil Bathla <sahilbathla1@gmail.com>
2013-08-13 12:19:40 +0530

    Pascal triangle

--- Basic_Ruby/Exercise 11/bin/pascal_triangle.rb
+++ Basic_Ruby/Exercise 11/bin/pascal_triangle.rb
@@ -2,15 +2,15 @@
 def factorial(number) 
    number >= 1 ? number = number * factorial(number - 1) : 1
 end
-
-if limit = gets.to_i 
-  for i in 0..limit
-    for j in 0..i
-      print factorial(i)/(factorial(j)*factorial(i-j))," "
-    end
-    puts
+limit = 0
+limit = gets.to_i while(limit == 0)
+for i in 0..limit
+  for j in 0..i
+    print factorial(i)/(factorial(j)*factorial(i-j))," "
   end
+  puts
 end
+
 
 
 

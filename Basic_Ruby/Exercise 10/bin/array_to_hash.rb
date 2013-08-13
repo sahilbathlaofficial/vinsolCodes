e57306afaa07a9d013fb7ea5f70b431c00555736
Sahil Bathla <sahilbathla1@gmail.com>
2013-08-13 11:00:06 +0530

    Changes

--- Basic_Ruby/Exercise 10/bin/array_to_hash.rb
+++ Basic_Ruby/Exercise 10/bin/array_to_hash.rb
@@ -1,3 +1,3 @@
 #! /usr/bin/ruby
 require_relative '../lib/array'
-puts [1,"sahil",3,[2,3,4,4],{'2'=>3,'a'=>7}].to_hash
+puts [1,"sahil",3,"Sold",756,1221,12].to_hash
--- Basic_Ruby/Exercise 10/lib/array.rb
+++ Basic_Ruby/Exercise 10/lib/array.rb
@@ -1,22 +1,13 @@
 class Array
   def to_hash
-    size = 0; 
-    returned_hash = self.inject( Hash.new{|hash,key| hash[key] = Array.new}) do |hash,element|
-      if ( element.is_a? Enumerable )
-        size = element.size
-      else
-         size = element.to_s.size
-      end
-      if size.odd? 
-        if hash["odd"][size] == nil then hash["odd"][size] = [element] else hash["odd"][size] << element end
-      else
-        if hash["even"][size] ==  nil then hash["even"][size] = [element] else hash["even"][size] << element end
-      end
+    returned_hash = self.inject( Hash.new{|hash,key| hash[key] = []} ) do |hash,element|
+      size = element.to_s.size
+      type = size.odd? ? "odd" : "even"
+      hash[type][size] ||= [] << element
       hash
     end
     returned_hash.each_value { |value| value.delete(nil)}
-    returned_hash
   end
 end
-print [1,2,"mold",[2,3,4,5],735,"as"].to_hash if __FILE__ == $0
+print [1,2,"mold",735,"as"].to_hash if __FILE__ == $0
 

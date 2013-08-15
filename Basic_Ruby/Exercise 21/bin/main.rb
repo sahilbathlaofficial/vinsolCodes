fc3866a303014a1707acfc71781149bfdd82856e
Sahil Bathla <sahilbathla1@gmail.com>
2013-08-16 02:06:45 +0530

    changes

--- Basic_Ruby/Exercise 21/bin/main.rb
+++ Basic_Ruby/Exercise 21/bin/main.rb
@@ -5,6 +5,10 @@ begin
   raise "Error Converting it to a number" if  (!(number.eql?"0") && (number.to_i) == 0)
   number = number.to_i
   raise "Negative numbers not allowed" if number < 0
+  rescue Exception
+  puts "#{$!} Exiting Program ... Press enter"
+  gets
+  abort
 end
 print "Factorial is: ", number > 1 ?  (1..number).factorial : "1","\n"
 

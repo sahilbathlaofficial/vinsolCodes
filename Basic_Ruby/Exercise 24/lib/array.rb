061527dbc999433a624f7f067ce2a6a52d456c5a
Sahil Bathla <sahilbathla1@gmail.com>
2013-08-16 14:30:28 +0530

    changes

--- Basic_Ruby/Exercise 24/lib/array.rb
+++ Basic_Ruby/Exercise 24/lib/array.rb
@@ -1,7 +1,8 @@
 class Array
   def reverse_iterate
-    for i in 1..size
-      yield self[size - i]
+    size_of_self = size
+    for i in 1..size_of_self
+      yield self[size_of_self - i]
     end
   end
 end

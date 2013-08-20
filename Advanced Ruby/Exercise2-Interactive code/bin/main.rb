f940ef55c4d85baf3469f23e84bcac09a1682b16
Sahil Bathla <sahilbathla1@gmail.com>
2013-08-20 16:30:49 +0530

    Interactive Code Exercise

--- Advanced Ruby/Exercise2-Interactive code/bin/main.rb
+++ Advanced Ruby/Exercise2-Interactive code/bin/main.rb
@@ -1,6 +1,6 @@
 #! /usr/etc/ruby
 require_relative "../lib/string"
-puts "\nPlease Enter The Code (Enter q/Q to exit or press Enter twice to evaluate your code\n\n".colorize(31)
+print "\nPlease Enter The Code (Enter q/Q to exit or press Enter twice to evaluate your code\n\n # ".colorize(31)
 while (line = gets) !~ /^q$/i
   code = (code || "") + line if line.size != 1
   if line.size == 1
@@ -15,4 +15,5 @@ while (line = gets) !~ /^q$/i
   	break if (line = gets) !~ /^yes|y$/i
     puts "\nPlease Enter The Code (Enter q/Q to exit or press Enter twice to evaluate your code\n\n".colorize(31)
   end
+  print "# ".colorize(31)
 end

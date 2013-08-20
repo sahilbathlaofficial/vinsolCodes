365d65cb138bdfef0fe5df3e6551ecde509e8405
Sahil Bathla <sahilbathla1@gmail.com>
2013-08-20 22:57:51 +0530

    Dynamic method using eval

--- Advanced Ruby/Exercise3-Program-from-command-line/lib/object.rb
+++ Advanced Ruby/Exercise3-Program-from-command-line/lib/object.rb
@@ -2,7 +2,11 @@ class Object
   def make_method(method_name, code)
     begin
     raise "Method name not proper" if method_name !~ /^[a-z][^\s]*$/
-      eval "def #{ method_name } \n #{ code } \n end"
+      instance_eval %{
+        def #{ method_name } 
+          #{ code } 
+        end
+      }
     rescue Exception
       abort "Quiting Program"
     end

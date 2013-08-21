003a069460278724dbf015979c9fdee685ef0c90
Sahil Bathla <sahilbathla1@gmail.com>
2013-08-21 13:10:54 +0530

    Passing block to yield

--- Basic_Ruby/Exercise 11/lib/integer.rb
+++ Basic_Ruby/Exercise 11/lib/integer.rb
@@ -3,8 +3,8 @@ class Integer
    number >= 1 ? number = number * factorial(number - 1) : 1
   end
 
-  def pascal_triangle(limit)
-    limit.times do |i|
+  def pascal_triangle
+    self.times do |i|
       (i+1).times do |j|
         yield factorial(i)/(factorial(j)*factorial(i-j)) 
       end
@@ -13,6 +13,6 @@ class Integer
   end
 
   def pascal_create
-    pascal_triangle(self) { |value| print value, " " }
+    pascal_triangle { |value| print value, " " }
   end
 end

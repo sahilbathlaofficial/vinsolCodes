e055a2ef637e6031fbda527782926cb761854955
Sahil Bathla <sahilbathla1@gmail.com>
2013-08-21 13:07:13 +0530

    Passing block to yield

--- Basic_Ruby/Exercise 11/bin/main.rb
+++ Basic_Ruby/Exercise 11/bin/main.rb
@@ -1,8 +1,8 @@
 #! /usr/bin/ruby
 require_relative "../lib/integer"
 limit = 0
-while(limit == 0)
+while(limit <= 0)
   puts "Enter the number of rows for pascal triangle"
   limit = gets.to_i 
 end
-limit.pascal_triangle { |value| print value }
+limit.pascal_create
--- Basic_Ruby/Exercise 11/lib/integer.rb
+++ Basic_Ruby/Exercise 11/lib/integer.rb
@@ -3,12 +3,16 @@ class Integer
    number >= 1 ? number = number * factorial(number - 1) : 1
   end
 
-  def pascal_triangle
-    self.times do |i|
+  def pascal_triangle(limit)
+    limit.times do |i|
       (i+1).times do |j|
-        yield "#{ factorial(i)/(factorial(j)*factorial(i-j)) } "
+        yield factorial(i)/(factorial(j)*factorial(i-j)) 
       end
-    yield "\n"
+    puts 
     end
+  end
+
+  def pascal_create
+    pascal_triangle(self) { |value| print value, " " }
   end
 end

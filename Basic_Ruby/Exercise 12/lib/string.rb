b2757f17ff9bd1a6db8b0e99aea7f1cc873dc22d
Sahil Bathla <sahilbathla1@gmail.com>
2013-08-21 12:14:46 +0530

    Digits range from 0 to 9 instead of 1 to 9

--- Basic_Ruby/Exercise 12/lib/string.rb
+++ Basic_Ruby/Exercise 12/lib/string.rb
@@ -9,8 +9,8 @@ class String
     digits_count = find_letters_in_range('0'..'9')
     special_symbols_count = gsub(' ', '').length - lower_case_count - upper_case_count - digits_count
 
-    puts "Lower case letters = #{ lower_case_count } "
-    puts "Upper case letters = #{ upper_case_count } "
+    puts "Lower case letters = #{ lower_case_count }"
+    puts "Upper case letters = #{ upper_case_count }"
     puts "Digits = #{ digits_count }"
     puts "Special letters = #{ special_symbols_count }"
   end  

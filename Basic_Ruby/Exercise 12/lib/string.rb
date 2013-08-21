2d6bb5c160c13938e73839ae7419a7f3c8b7dbbd
Sahil Bathla <sahilbathla1@gmail.com>
2013-08-21 12:06:15 +0530

    Spacing issue resolved

--- Basic_Ruby/Exercise 12/lib/string.rb
+++ Basic_Ruby/Exercise 12/lib/string.rb
@@ -7,7 +7,7 @@ class String
     lower_case_count = find_letters_in_range('a'..'z')
     upper_case_count = find_letters_in_range('A'..'Z')
     digits_count = find_letters_in_range('1'..'9')
-    special_symbols_count = gsub(' ','').length - lower_case_count - upper_case_count - digits_count
+    special_symbols_count = gsub(' ', '').length - lower_case_count - upper_case_count - digits_count
 
     puts "Lower case letters = #{ lower_case_count } "
     puts "Upper case letters = #{ upper_case_count } "

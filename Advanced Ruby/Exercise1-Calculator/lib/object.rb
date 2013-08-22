a13bc3d3560f15cfa807496c9e5bf9edadd4d774
Sahil Bathla <sahilbathla1@gmail.com>
2013-08-22 13:03:15 +0530

    Class Calculator added

--- Advanced Ruby/Exercise1-Calculator/lib/object.rb
+++
@@ -1,6 +0,0 @@
-class Object
-  def calculate (operand_1,operator,operand_2)
-    raise "Only numbers" if(!(operand_1.is_a?Numeric) || !(operand_2.is_a?Numeric))
-    operand_1.send(operator.to_sym,operand_2)
-  end
-end

8eb57a083ea52705735a26c641051c9485dd1c5d
Sahil Bathla <sahilbathla1@gmail.com>
2013-08-23 23:43:24 +0530

    Spacing issues solved, Demorgans rule applied, And class method made

--- Advanced Ruby/Exercise1-Calculator/lib/calculator.rb
+++ Advanced Ruby/Exercise1-Calculator/lib/calculator.rb
@@ -1,6 +1,6 @@
 class Calculator
-  def calculate(operand_1, operator, operand_2)
-    raise "Only numbers" if(!(operand_1.is_a?Numeric) || !(operand_2.is_a?Numeric))
-    operand_1.send(operator.to_sym, operand_2)
+  def self.calculate(operand_1, operator, operand_2)
+    raise "Only numbers" if !((operand_1.is_a? Numeric) && (operand_2.is_a? Numeric))
+    operand_1.send(operator, operand_2)
   end
 end

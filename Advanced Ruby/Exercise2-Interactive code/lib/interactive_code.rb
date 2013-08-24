99ec99fe3fcfeb8f956d48ade63e09c9fb9564ad
Sahil Bathla <sahilbathla1@gmail.com>
2013-08-24 11:27:37 +0530

    Class Interactive code with options to generate,evaluate and reset code

---
+++ Advanced Ruby/Exercise2-Interactive code/lib/interactive_code.rb
@@ -0,0 +1,32 @@
+require_relative 'colorize'
+class InteractiveCode
+
+  include Colorize
+
+  def initialize 
+    reset
+  end
+
+  def reset
+    @code = ''
+  end 
+
+  def generate_code(terminating_str, evaluating_str = "\n", shell_symbol = "#" )
+    loop do
+      print string_color(" #{ shell_symbol } ", 31)
+      line = gets 
+      @code += line
+      abort if @code.chomp =~ /^#{ terminating_str }$/i
+      break if line == evaluating_str 
+    end
+  end
+
+  def evaluate
+    begin
+  	  eval @code 
+  	rescue Exception => code_execution_error
+  	  code_execution_error
+  	end
+  end
+
+end

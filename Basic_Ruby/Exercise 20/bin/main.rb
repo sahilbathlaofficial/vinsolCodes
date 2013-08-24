25d0458dd8bb2bf54a6146cfb9eaab2412c6a7a2
Sahil Bathla <sahilbathla1@gmail.com>
2013-08-24 12:16:19 +0530

    Initializing values using block

--- Basic_Ruby/Exercise 20/bin/main.rb
+++ Basic_Ruby/Exercise 20/bin/main.rb
@@ -3,13 +3,14 @@ require 'prime'
 require_relative '../lib/interest'
 
 puts "Enter Principal"
-principal = gets while (principal = principal.to_i) == 0
+principal = gets while(principal = principal.to_i) == 0
 puts "Enter Time"
-time = gets while (time = time.to_i) == 0
+time = gets while(time = time.to_i) == 0
 
 #interest = Interest.new(Principal,Time,Rate)
-interest = Interest.new(principal,time,10) { |value| puts "Difference = ",value.abs.round(2) }
-puts interest.difference_in_interest
-
-
-
+interest = Interest.new(principal, time, 10) do 
+  @principal = principal
+  @time = time
+  @rate = 10
+end
+puts "Difference in interest- #{ interest.difference_in_interest.round(2) }"
--- Basic_Ruby/Exercise 20/lib/interest.rb
+++ Basic_Ruby/Exercise 20/lib/interest.rb
@@ -1,18 +1,21 @@
 class Interest
-  def initialize (principal,time,rate = 10,&diff)
-    @principal = principal
-    @time = time
-    @rate = rate
-    @diff = diff
+
+  def initialize (principal, time, rate = 10, &initialize_block)
+    instance_eval &initialize_block
   end
+
   def difference_in_interest
-    @diff.call(compound_interest - simple_interest)
+    compound_interest - simple_interest
   end
+
   private 
+
   def simple_interest
     @principal * (@rate/100.0) * @time
   end
+
   def compound_interest
     @principal * (((@rate/100.0) + 1) ** @time) - @principal
   end
+
 end

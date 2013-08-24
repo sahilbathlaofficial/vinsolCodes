ffb273eadd1d026a4074942613dd9544c2067379
Sahil Bathla <sahilbathla1@gmail.com>
2013-08-24 16:32:07 +0530

    NonNumeric raised and custom non_numeric class required

--- Advanced Ruby/Exercise1-Calculator/lib/calculator.rb
+++ Advanced Ruby/Exercise1-Calculator/lib/calculator.rb
@@ -1,6 +1,7 @@
+require_relative 'not_numeric'
 class Calculator
   def self.calculate(operand_1, operator, operand_2)
-    raise StandardError, "Only numbers" if !((operand_1.is_a? Numeric) && (operand_2.is_a? Numeric))
+    raise NotNumeric if !((operand_1.is_a? Numeric) && (operand_2.is_a? Numeric))
     operand_1.send(operator, operand_2)
   end
 end

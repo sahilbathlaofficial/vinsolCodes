821c8b9b986de453d5df83c51e60ae01311e785f
Sahil Bathla <sahilbathla1@gmail.com>
2013-08-24 11:53:55 +0530

    Spacing issues resolved

--- Basic_Ruby/Exercise 10/bin/main.rb
+++ Basic_Ruby/Exercise 10/bin/main.rb
@@ -1,3 +1,3 @@
 #! /usr/bin/ruby
 require_relative '../lib/array'
-puts [1,"sahil",3,"Sold",756,1221,12].to_hash
+puts [1, "sahil", 3, "Sold", 756, 1221, 12].to_hash
--- Basic_Ruby/Exercise 10/lib/array.rb
+++ Basic_Ruby/Exercise 10/lib/array.rb
@@ -1,13 +1,13 @@
 class Array
   def to_hash
-    returned_hash = self.inject( Hash.new{|hash,key| hash[key] = []} ) do |hash,element|
+    returned_hash = self.inject(Hash.new{ |hash,key| hash[key] = [] }) do |hash,element|
       size = element.to_s.size
       type = size.odd? ? "odd" : "even"
-      (hash[type][size] ||= []) << element
+      hash[type][size] ||= []
+      hash[type][size] << element
       hash
     end
-    returned_hash.each_value { |value| value.delete(nil)}
+    returned_hash.each_value { |value| value.delete(nil) }
   end
 end
-print [1,2,"mold",735,"as"].to_hash if __FILE__ == $0
-
+print [1, 2, "mold", 735, "as"].to_hash if __FILE__ == $0

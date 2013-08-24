ae373c033c279b92014e00dc191564c756cb4f63
Sahil Bathla <sahilbathla1@gmail.com>
2013-08-24 12:43:23 +0530

    Spacing issues resolved

--- Basic_Ruby/Exercise 22/lib/name.rb
+++ Basic_Ruby/Exercise 22/lib/name.rb
@@ -1,15 +1,18 @@
 class Name
   attr_reader :first_name,:last_name
+
   def initialize
     begin
       @first_name = get_name
-      @last_name = get_name( "last")
+      @last_name = get_name("last")
     end
   end
+
   def get_name(type = "first")
-    puts "Enter #{type} name"
+    puts "Enter #{ type } name"
     name = gets.chomp
-    raise Exception,"Name can't have Null values",caller[0] if (name.size == 0 )
-    raise Exception,"Initial letter not capital",caller[0] if (name.strip[0] !~ /[A-Z]/)
+    raise Exception, "Name can't have Null values", "" if (name.size == 0 )
+    raise Exception, "Initial letter not capital", "" if (name.strip[0] !~ /[A-Z]/)
   end
+
 end

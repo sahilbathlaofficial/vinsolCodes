416edd27bf6119ee85ec36a0c70ade7ddf013526
Sahil Bathla <sahilbathla1@gmail.com>
2013-08-26 17:13:34 +0530

    using boolean for import duty and sales tax

--- Basic_Ruby/Exercise 25/bin/main.rb
+++ Basic_Ruby/Exercise 25/bin/main.rb
@@ -3,30 +3,43 @@ require_relative '../lib/product'
 require_relative '../lib/invoice'
 
 product_array = []
+
 loop do
+
   print "Name of the product : " 
   product_name = gets.chomp
   redo unless(product_name.size != 0) 
+
   while true  
-    print "import_duty?(yes/no) :" 
+    print "import_duty?:" 
     import_duty = gets.chomp
-    break if (import_duty =~ /^(yes|no)$/i)
+    if(import_duty =~ /^(yes|no)$/i)
+      import_duty = (import_duty =~ /^yes$/i) ? true : false 
+      break
+    end
   end
+
   while true
     sales_tax = "no" if product_name =~ /book|food|medicine/i
-    break if (sales_tax =~ /^(yes|no)$/i)
+    if(sales_tax =~ /^(yes|no)$/i)
+      sales_tax = (sales_tax =~ /^yes$/i) ? true : false 
+      break
+    end
     print "Exempted from tax? :" 
     sales_tax = gets.chomp
   end
+
   while true
     print "Price: "
     price = gets.to_f
     break if (price != 0)
   end
-  product_array << Product.new(product_name,import_duty,sales_tax,price)
+
+  product_array << Product.new(product_name, price, import_duty, sales_tax)
   puts "Do you want to continue?" 
   continue = gets
   break if(continue !~ /^(y|yes)$/i)
+
 end
 invoice = Invoice.new(product_array)
 invoice.generate_invoice

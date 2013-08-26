6fd2df3375a94b676c97ae9395c89251cdb7c0fa
Sahil Bathla <sahilbathla1@gmail.com>
2013-08-26 17:11:17 +0530

    sum_total => calculate_total,attr_accessor used,calculating price only while generating invoice

--- Basic_Ruby/Exercise 25/lib/invoice.rb
+++ Basic_Ruby/Exercise 25/lib/invoice.rb
@@ -1,18 +1,24 @@
-require_relative 'print.rb'
+require_relative 'print_table'
+
 class Invoice
-  attr_reader :products,:total_price
+
+  attr_accessor :products, :total_price
+
   def initialize (products)
     @products = products
-    @total_price = sum_price.to_i
   end
-  def sum_price
-    @products.inject(0) { |sum,product| sum += product.taxed_price }
+
+  def calculate_total
+    @total_price  = products.inject(0) { |sum,product| sum += product.taxed_price }
   end
+
   def generate_invoice
+    calculate_total
     data = []
-    @products.each do |data_row| 
-      data << [data_row.product,data_row.price.round(2),data_row.sales_tax,data_row.import_duty,data_row.taxed_price.round(2)] 
+    products.each do |data_row| 
+      data << [data_row.name, data_row.price.round(2), data_row.sales_tax, data_row.import_duty, data_row.taxed_price.round(2)] 
     end
-    Print.print_table(["Name","Price","Sales Tax","Import Duty","Taxed Price"],data,14,@total_price)
+    PrintTable.print_table(["Name", "Price", "Sales Tax", "Import Duty", "Taxed Price"], data, 14, "Grand Total : #{ total_price }")
   end
+
 end

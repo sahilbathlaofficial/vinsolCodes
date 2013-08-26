368ec1d022f1ece7cb64c2564a3b143510708b8e
Sahil Bathla <sahilbathla1@gmail.com>
2013-08-26 19:44:22 +0530

    Separate class of Reservation used

--- Basic_Ruby/Hotel_reservation/bin/main.rb
+++ Basic_Ruby/Hotel_reservation/bin/main.rb
@@ -1,16 +1,16 @@
 #! /usr/bin/ruby
-require_relative '../lib/hotel_list'
-
-
+require_relative '../lib/hotel'
+require_relative '../lib/reservation'
 
 puts "Enter check in date (yyyy-mm-dd)"
 check_in = gets.chomp
 puts "Enter check out date (yyyy-mm-dd)"
 check_out = gets.chomp
-hotels =  HotelList.get_hotel_list
+hotels =  Hotel.get_hotel_list
 puts "\n"
-abort("You can't go back in time :P ") if(check_out < check_in)
-hotels.each{ |hotel| puts hotel.get_reservation(check_in,check_out) } 
+abort("You can't go back in time ") if(check_out < check_in)
+reservation = Reservation.new(check_in, check_out)
+hotels.each{ |hotel| puts reservation.calculate_rent(hotel) } 
 
 
 

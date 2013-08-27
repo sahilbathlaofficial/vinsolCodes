118af2b0d55d7892fc320dc4b70ca96040845b08
Sahil Bathla <sahilbathla1@gmail.com>
2013-08-27 11:28:01 +0530

    using compact! instead of delete(nil)

--- Basic_Ruby/Exercise 10/lib/array.rb
+++ Basic_Ruby/Exercise 10/lib/array.rb
@@ -7,7 +7,7 @@ class Array
       hash[type][size] << element
       hash
     end
-    returned_hash.each_value { |value| value.delete(nil) }
+    returned_hash.each_value { |value| value.compact! }
   end
 end
 print [1, 2, "mold", 735, "as"].to_hash if __FILE__ == $0

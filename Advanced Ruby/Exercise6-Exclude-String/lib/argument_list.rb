bdb48fc41d3c1823d2afa39f921c28901066c902
Sahil Bathla <sahilbathla1@gmail.com>
2013-09-25 13:45:21 +0530

    Cases added for variable/default arguments

--- Advanced Ruby/Exercise6-Exclude-String/lib/argument_list.rb
+++ Advanced Ruby/Exercise6-Exclude-String/lib/argument_list.rb
@@ -1,29 +1,70 @@
-module ArgumentList
+class ArgumentList
+  attr_accessor :instance, :method_name, :arity, :arguments
+  def initialize(params)
+    @instance = params[:instance]
+    @method_name = params[:method_name] 
+    @arity = params[:arity]
+    @arguments = []
+  end
 
-  def collect_arguments(arity)
-    args = []
+  def generate_arguments
     #if variable number of arguments
-    if arity == -1
-      i = 0
-      repeat = ''
-      begin
-        args << input_argument(i)
-        i += 1
-        puts "Want more arguments?(Yes/No)"
-        repeat = gets.chomp
-      end while (repeat =~ /^yes$/i)
+    if arity < 0
+      self.arguments = fixed_arguments_list(number_of_args: arity.abs - 1) if arity != -1
+      self.arguments += has_variable_arguments? ? variable_arguments_input : optional_arguments_list
     else
       #if fixed number of arguments
-      arity.times do |i|
-        args << input_argument(i)
-      end
+      self.arguments = fixed_arguments_list(number_of_args: arity) if arity > 0
+    end
+    arguments
+  end
+
+
+  def has_variable_arguments?
+    instance.method(method_name.to_sym).parameters.any?{ |array| array[0] == :rest } 
+  end
+
+  ## In case of variable arguments ##
+  def variable_arguments_input
+    args = []
+    repeat = ''
+    puts "Enter Variable arguments"
+    begin
+      puts "Want to enter more arguments?(Yes/No)"
+      repeat = gets.chomp
+      break if !(repeat =~ /^yes$/i)
+      args << input_argument
+    end while (repeat =~ /^yes$/i)
+    args
+  end
+
+  ## In case of fixed/required arguments ####
+  def fixed_arguments_list(arguments_hash)
+    args = []
+    print "There are ", arguments_hash[:number_of_args], " Required arguments\n"
+    arguments_hash[:number_of_args].times do 
+      args << input_argument
+    end
+    args
+  end
+  ## In case default arguments ###
+  def optional_arguments_list
+    args = []
+    repeat =  ''   
+    optional_arg_count = instance.method(method_name.to_sym).parameters.count{ |array| array[0] == :opt }
+    print "Enter ", optional_arg_count, " optional arguments\n"
+    optional_arg_count.times do 
+      puts "Want to enter more optional arguments?(Yes/No)"
+      repeat = gets.chomp
+      break if !(repeat =~ /^yes$/i)
+      args << input_argument
     end
     args
   end
 
-  def input_argument(i)
-    puts "Input argument #{ i + 1 }"
-    gets.chomp
+  def input_argument
+    puts "Input next argument "
+    eval gets.chomp
   end
 
 end
--- Advanced Ruby/Exercise6-Exclude-String/lib/derived_string.rb
+++ Advanced Ruby/Exercise6-Exclude-String/lib/derived_string.rb
@@ -1,20 +1,24 @@
 require_relative 'argument_list'
 
 class DerivedString < String
-  
-  include ArgumentList
+
+  def caesar_encrypt(number, args="encoded")
+    each_byte{ |ch|  print ch + number.to_i }
+    # do something with args
+  end
 
   def exclude? substring
     !include? substring
   end
 
   def test_a_method
-    puts "Enter the method you want to run on this string object"
+    puts "Enter the method you want to run on this string object\n"
     method_name = gets.chomp
     if(respond_to? method_name)
-      args = []
+      puts "\nEnter value in proper format i.e. (String in quotes)\n\n"
       arity = method(method_name.to_sym).arity
-      args = collect_arguments(arity)
+      argument_list = ArgumentList.new(method_name: method_name, instance: self, arity: arity)
+      args = argument_list.generate_arguments
       puts instance_eval  "self.#{method_name}(*args) " 
     else
       puts "Method not defined for this object"

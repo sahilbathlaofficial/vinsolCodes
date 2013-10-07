c608971b80ba50a674c682e83b04b665cfbdf5f2
Chhavi Khandelwal <chhavi@vinsol.com>
2013-10-07 15:58:35 +0530

    Error message added in whole authentication

--- Rails/vTweet/app/controllers/controller_devise/registrations_controller.rb
+++ Rails/vTweet/app/controllers/controller_devise/registrations_controller.rb
@@ -1,11 +1,15 @@
 class ControllerDevise::RegistrationsController < Devise::RegistrationsController
-  before_filter :configure_permitted_parameters, :only => [:create]
+  include AuthenticationErrorGenerator
+  before_filter :only => :create do |filter|
+    filter.generate_error("Sign Up error")
+    filter.configure_permitted_parameters
+  end
   layout "sign_in_sign_up"
 
   protected
 
     def configure_permitted_parameters
-      devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password) }
+      devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :confirm_password) }
     end
     
 end
--- Rails/vTweet/app/controllers/controller_devise/sessions_controller.rb
+++ Rails/vTweet/app/controllers/controller_devise/sessions_controller.rb
@@ -2,7 +2,7 @@ class ControllerDevise::SessionsController < Devise::SessionsController
   include AuthenticationErrorGenerator
   layout "sign_in_sign_up"
 
-  before_filter [:create] do |filter|
+  before_filter :only  => [:create] do |filter|
     filter.generate_error("Invalid email and password combination")
   end
 

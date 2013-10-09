4a60b0765195c339af37d80832e3ee5fd8eb9cea
Chhavi Khandelwal <chhavi@vinsol.com>
2013-10-09 11:07:10 +0530

    attr_writer added to check the name in proper format before assigning to instance variable

--- Basic_Ruby/Exercise 22/lib/name.rb
+++ Basic_Ruby/Exercise 22/lib/name.rb
@@ -2,21 +2,29 @@ require_relative 'not_null'
 require_relative 'not_capitalize'
 
 class Name
-  attr_reader :first_name, :last_name
 
-  def initialize
-    begin
-      @first_name = get_name
-      @last_name = get_name("last")
+  def self.my_attr_writer(*variables)
+    variables.each do |variable|
+      define_method("#{ variable.to_s }=") do |attribute_value|
+        instance_variable_set('@' + variable.to_s, attribute_value) if attribute_in_proper_format?(attribute_value) 
+      end
     end
   end
 
-  def get_name(type = "first")
-    puts "Enter #{ type } name"
-    name = gets.chomp
-    raise NotNull  if(name.empty?)
-    raise NotCapitalize if(name[0] !~ /[A-Z]/)
-    name
+  attr_reader :firstname, :lastname
+  my_attr_writer :firstname, :lastname 
+
+  def initialize(name)
+    begin
+      self.firstname = name[:firstname]
+      self.lastname = name[:lastname]
+    end
+  end
+
+  def attribute_in_proper_format?(attribute_value)
+    raise NotNull  if(attribute_value.empty?)
+    raise NotCapitalize if(attribute_value[0] !~ /[A-Z]/)
+    true
   end
 
 end

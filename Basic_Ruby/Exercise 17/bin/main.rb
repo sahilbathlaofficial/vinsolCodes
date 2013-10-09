2fd60f67a7fd3cda6024a0e44ac61d917633ada5
Chhavi Khandelwal <chhavi@vinsol.com>
2013-10-09 11:34:55 +0530

    DRY removed, spacing issues resolved

--- Basic_Ruby/Exercise 17/bin/main.rb
+++ Basic_Ruby/Exercise 17/bin/main.rb
@@ -1,16 +1,13 @@
 #! /usr/bin/ruby
-require "time"
-require_relative "../lib/string.rb"
-puts "Enter 1st time in (hh:mm:ss) format" 
-time_first = gets.chomp
-abort("Bad format") if !time_first.check_valid?
-puts "Enter 2nd time in (hh:mm:ss) format" 
-time_second = gets.chomp
-abort("Bad format") if !time_second.check_valid?
-time_first.add_time(time_second)
+require_relative '../lib/time.rb'
+require_relative '../lib/string.rb'
 
+def input_time(index)
+  puts "Enter #{ index } time in (hh:mm:ss) format" 
+  gets
+end
 
-
-
-
-
+time_first = input_time('1st').to_time
+time_second = input_time('2nd').to_time
+added_time = time_first.add_time(time_second)
+print "\n", added_time[:Days], ' Days & ', added_time[:Hours], ':', added_time[:Mins], ':', added_time[:Secs], "\n"

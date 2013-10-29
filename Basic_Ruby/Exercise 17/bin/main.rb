f08f6cf311b4c3e4c3f705c063c626a07e7ff6bf
Sahil Bathla <sahil@vinsol.com>
2013-10-30 00:04:21 +0530

    Keys changed to be in downcase

--- Basic_Ruby/Exercise 17/bin/main.rb
+++ Basic_Ruby/Exercise 17/bin/main.rb
@@ -10,4 +10,4 @@ end
 time_first = input_time('1st').to_time
 time_second = input_time('2nd').to_time
 added_time = time_first.add_time(time_second)
-print "\n", added_time[:Days], ' Days & ', added_time[:Hours], ':', added_time[:Mins], ':', added_time[:Secs], "\n"
+print "\n", added_time[:days], ' Days & ', added_time[:hours], ':', added_time[:mins], ':', added_time[:secs], "\n"

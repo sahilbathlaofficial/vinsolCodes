2deefeaa8dcdfea46901366571939b2ee9e5af6e
Sahil Bathla <sahil@vinsol.com>
2013-10-30 00:05:17 +0530

    check_valid? converted to time_valid?

--- Basic_Ruby/Exercise 17/lib/string.rb
+++ Basic_Ruby/Exercise 17/lib/string.rb
@@ -1,12 +1,12 @@
 require "time"
 class String
 
-  def check_valid?(time)
+  def time_valid?(time)
    time =~ /([01]?[0-9]|2[0-3]):([0-5][0-9]|6[0]):([0-5][0-9]|6[0])/
   end
 
   def to_time
-    Time.parse(self) if check_valid?(self)
+    Time.parse(self) if time_valid?(self)
   end
 
 end

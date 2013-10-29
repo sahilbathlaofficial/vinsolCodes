cccdf9fd240197a031839d27d65edac590958558
Sahil Bathla <sahil@vinsol.com>
2013-10-30 00:06:08 +0530

    Downcased symbols

--- Basic_Ruby/Exercise 17/lib/time.rb
+++ Basic_Ruby/Exercise 17/lib/time.rb
@@ -3,7 +3,7 @@ class Time
   def add_time(other_time)  
     new_time = self + (other_time - Time.parse("00:00:00"))
     days = (Time.now.day == new_time.day) ? 0 : 1
-    return({ Days: days, Hours: new_time.hour, Mins: new_time.min, Secs: new_time.sec })
+    return({ days: days, hours: new_time.hour, mins: new_time.min, secs: new_time.sec })
   end
   
 end
